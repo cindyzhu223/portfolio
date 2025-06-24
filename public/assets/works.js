eae728a0d8a87a24f68f715c6a9b587c9fdcdea0
alex-lin64 <alex_lin@brown.edu>
2025-06-24 18:35:09 -0400

    fixed major bugs

--- public/assets/works.js
+++ public/assets/works.js
@@ -19,6 +19,106 @@ const worksData = [
     title: "test",
     subtitle: "l3",
   },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+  {
+    imageUrl: "/test2.jpg",
+    title: "test",
+    subtitle: "l3s sdfsd sdfasd fa",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3 sdfsd s fsdf ss fss s ssd asdf sdsfsd fsdfsdf ",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+  {
+    imageUrl: "/test2.jpg",
+    title: "test",
+    subtitle: "l3s sdfsd sdfasd fa",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3 sdfsd s fsdf ss fss s ssd asdf sdsfsd fsdfsdf ",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+    {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+  {
+    imageUrl: "/test2.jpg",
+    title: "test",
+    subtitle: "l3s sdfsd sdfasd fa",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3 sdfsd s fsdf ss fss s ssd asdf sdsfsd fsdfsdf ",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+    {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+  {
+    imageUrl: "/test2.jpg",
+    title: "test",
+    subtitle: "l3s sdfsd sdfasd fa",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3 sdfsd s fsdf ss fss s ssd asdf sdsfsd fsdfsdf ",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+    {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
+  {
+    imageUrl: "/test2.jpg",
+    title: "test",
+    subtitle: "l3s sdfsd sdfasd fa",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3 sdfsd s fsdf ss fss s ssd asdf sdsfsd fsdfsdf ",
+  },
+  {
+    imageUrl: "/home.jpg",
+    title: "test",
+    subtitle: "l3",
+  },
 ];
 
 export default worksData;
